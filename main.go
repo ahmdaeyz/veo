d6e57a5f79f0a014a29185bdda47a6a646d74657
Ahmed Aboelyazeed <ahmedalarabe5@gmail.com>
2019-04-25 14:13:40 +0200

    Fix: fix init function

--- main.go
+++ main.go
@@ -51,10 +51,11 @@ func init() {
 	conf.Use(configure.NewFlag())
 	conf.Use(configure.NewEnvironment())
 	conf.Use(configure.NewJSONFromFile("./config.json"))
+	var collection *mongo.Collection
 	c := colly.NewCollector(
 		colly.UserAgent("Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.109 Safari/537.36"),
 	)
-	ctx, _ = context.WithTimeout(context.Background(), 10*time.Second)
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
 	db, err := mongo.Connect(ctx, options.Client().ApplyURI(*mongoURI))
 	if err != nil {
 		log.Fatal(err)
