0fd11ec51f7a2158202077c7f72b19366d005c41
Ahmed Aboelyazeed <ahmedalarabe5@gmail.com>
2019-04-26 14:12:44 +0200

    Allow url revisit

--- main.go
+++ main.go
@@ -55,6 +55,7 @@ func init() {
 	conf.Use(configure.NewJSONFromFile("./config.json"))
 	c = colly.NewCollector(
 		colly.UserAgent("Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.109 Safari/537.36"),
+		colly.AllowURLRevisit(),
 	)
 }
 
