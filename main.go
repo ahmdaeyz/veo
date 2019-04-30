403c7e8cb92c45532dede01e8d5d8c62843cc83e
Ahmed Aboelyazeed <ahmedalarabe5@gmail.com>
2019-04-30 18:02:15 +0200

    Passing right argument to isVideo

--- main.go
+++ main.go
@@ -142,7 +142,7 @@ func messages(m messenger.Message, r *messenger.Response) {
 			r.Text("Please Share a Post that contains a video :)", messenger.ResponseType)
 		}
 	} else if strings.Contains(m.Text, "https://www.facebook.com") {
-		isVideo, err := isVideo(m.Attachments[0].URL)
+		isVideo, err := isVideo(strings.TrimSpace(m.Text))
 		if err != nil {
 			r.Text("Smth went wrong,Kindly try again", messenger.ResponseType)
 		}
